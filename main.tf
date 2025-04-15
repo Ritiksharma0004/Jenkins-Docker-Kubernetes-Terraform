7a7186211ad862889810139ef6fcae2a2478152f
Ritiksharma0004 <142799642+Ritiksharma0004@users.noreply.github.com>
2025-04-15 10:07:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "61bd38d2-51fa-4243-9689-7f55df510fc1"
-  tenant_id       = "b0b76d6e-14b7-4c08-8657-a19ef23b3b0b"
+  subscription_id = "75bc0460-a1cc-4e75-90ec-3387ed5a4f03"
+  tenant_id       = "ec8f417a-bd75-4849-8134-019406dfea6f"
   features {}
 }
 
@@ -20,11 +20,11 @@ variable "location" {
 }
 
 variable "resource_group_name" {
-  default = "myResourceGroup"
+  default = "myResourceGroup44"
 }
 
 variable "acr_name" {
-  default = "acrrathore01" 
+  default = "acrsharma01" 
 }
 
 variable "aks_cluster_name" {
